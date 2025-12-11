f830f4e13b10d1962a0efa151d3befab6dc977b7
Checkmate6659 <73489554+Checkmate6659@users.noreply.github.com>
2025-12-11 21:07:40 +0100

    ITS OF THE RIGHT COLOR UwU

--- src/main/java/com/breakingfemme/BreakingFemmeClient.java
+++ src/main/java/com/breakingfemme/BreakingFemmeClient.java
@@ -7,6 +7,9 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
 import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.util.Identifier;
 
@@ -25,6 +28,11 @@ public class BreakingFemmeClient implements ClientModInitializer {
                 0xC033C1FF) //alpha, r, g, b
         );
         BlockRenderLayerMap.INSTANCE.putFluids(RenderLayer.getTranslucent(), ModFluids.STILL_COPPER_SULFATE, ModFluids.FLOWING_COPPER_SULFATE);
+        ColorProviderRegistry.BLOCK.register((blockState, blockAndTintGetter, blockPos, i) -> {
+            if (i == 0)
+                return 0xC033C1FF;
+            return -1;
+        }, ModFluids.COPPER_SULFATE_CAULDRON);
 
         //nickel sulfate
         FluidRenderHandlerRegistry.INSTANCE.register(ModFluids.STILL_NICKEL_SULFATE, ModFluids.FLOWING_NICKEL_SULFATE,
--- src/main/java/com/breakingfemme/datagen/ModModelProvider.java
+++ src/main/java/com/breakingfemme/datagen/ModModelProvider.java
@@ -7,7 +7,6 @@ import com.breakingfemme.item.ModItems;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.block.Blocks;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
 import net.minecraft.data.client.Models;
