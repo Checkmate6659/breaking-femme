4f1fe08d00ee08458d0b852144c3148ae5332150
Checkmate6659 <73489554+Checkmate6659@users.noreply.github.com>
2025-12-17 17:28:05 +0100

    Added crafting recipe

--- src/main/java/com/breakingfemme/datagen/ModRecipeProvider.java
+++ src/main/java/com/breakingfemme/datagen/ModRecipeProvider.java
@@ -3,11 +3,13 @@ package com.breakingfemme.datagen;
 import java.util.List;
 import java.util.function.Consumer;
 
+import com.breakingfemme.block.ModBlocks;
 import com.breakingfemme.item.ModItems;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
@@ -37,5 +39,8 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         offerBlasting(exporter, List.of(ModItems.PULVERIZED_COPPER), RecipeCategory.MISC, Items.COPPER_INGOT, 0, 100, "copper_ingot");
         offerSmelting(exporter, List.of(ModItems.PULVERIZED_NICKEL), RecipeCategory.MISC, ModItems.NICKEL_INGOT, 0, 200, "nickel_ingot");
         offerBlasting(exporter, List.of(ModItems.PULVERIZED_NICKEL), RecipeCategory.MISC, ModItems.NICKEL_INGOT, 0, 100, "nickel_ingot");
+
+        //fermenter panels
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.FERMENTER_PANEL.asItem(), 16).input('#', ItemTags.PLANKS).input('-', Items.IRON_BARS).pattern("###").pattern("---").pattern("###").group("fermenter_panel").criterion(hasItem(Items.IRON_BARS), conditionsFromItem(Items.IRON_BARS)).offerTo(exporter);
     }
 }
