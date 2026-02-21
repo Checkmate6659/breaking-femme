748a7d050f5406d6357f70aa7239634125859b53
Checkmate6659 <73489554+Checkmate6659@users.noreply.github.com>
2026-02-21 11:12:44 +0100

    Made uniforms update more often (not enough tho); TODO: fix blur brightness bug

--- src/main/java/com/breakingfemme/mixin/AlteredVisionMixin.java
+++ src/main/java/com/breakingfemme/mixin/AlteredVisionMixin.java
@@ -3,12 +3,12 @@ package com.breakingfemme.mixin;
 import java.util.List;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.breakingfemme.BreakingFemme;
 import com.breakingfemme.KineticsAttachments;
 
 import net.minecraft.client.MinecraftClient;
@@ -19,6 +19,9 @@ import net.minecraft.util.Identifier;
 
 @Mixin(GameRenderer.class)
 public abstract class AlteredVisionMixin {
+	@Unique
+	private static final Identifier ALTERED_VISION_ID = new Identifier("shaders/post/altered_vision.json");
+
     @Invoker("disablePostProcessor") //TODO: doesn't actually need an invoker
 	public abstract void breakingfemme_invokeClearPostProcessor();
 
@@ -36,32 +39,38 @@ public abstract class AlteredVisionMixin {
 		float etoh = KineticsAttachments.getLevel(player, KineticsAttachments.ETHANOL);
         //float ach = KineticsAttachments.getLevel(player, KineticsAttachments.ACETALDEHYDE);
 
-		if((player.getWorld().getTime() & 15) != 0)
-			return;
-
 		if(etoh > 1.25f)
 		{
 			//get proper name with this? if its null then ofc its not bound.
 			//((GameRenderer)(Object)this).getPostProcessor().getName()
 
-			//FIXME: does this leak resources??
-			if(((GameRenderer)(Object)this).getPostProcessor() == null) //we do not want invokeSetPostProcessor to be called again and again
+			if((player.getWorld().getTime() & 1) == 0 && ((GameRenderer)(Object)this).getPostProcessor() == null) //we do not want invokeSetPostProcessor to be called every frame, that breaks the game
 			{
-				//TODO: set uniforms: PostEffectPass, line 73
 				//shader tutorial (GLSL, jsons): https://www.minecraftforum.net/forums/mapping-and-modding-java-edition/mapping-and-modding-tutorials/3201009-tutorial-vanilla-minecraft-shaders-creating-post
 				//https://docs.google.com/document/d/15TOAOVLgSNEoHGzpNlkez5cryH3hFF3awXL5Py81EMk/edit?tab=t.0
+				//fix for 1.20.1 shader loading bug
+				//https://discord.com/channels/507304429255393322/807617488313516032/1474509569739723015
+				//has some code that actually works
 				//invokeSetPostProcessor(Identifier.of("minecraft", "shaders/post/spider.json"));
-				breakingfemme_invokeSetPostProcessor(Identifier.of(BreakingFemme.MOD_ID, "shaders/post/altered_vision.json"));
+				breakingfemme_invokeSetPostProcessor(ALTERED_VISION_ID);
+			}
 
+			//however we do want to set the uniforms often
+			//why do they only get set every once in a while?
+			if(((GameRenderer)(Object)this).getPostProcessor() != null && ((GameRenderer)(Object)this).getPostProcessor().getName().equals(ALTERED_VISION_ID.toString())) //triggers after getting set to not be null => cant use if else
+			{
+				float blur_strength = (etoh - 1.25f) * 0.5f; //goes between 0 and 1: more drunk => see worse
+				if(blur_strength > 1.0f) blur_strength = 1.0f;
+				
 				//set uniforms
 				List<PostEffectPass> passes = ((PostEffectPassAccessor)(((GameRenderer)(Object)this).getPostProcessor())).breakingfemme$getPasses();
 
-				long time = player.getWorld().getTime();
-				passes.get(1).getProgram().getUniformByNameOrDummy("Radius").set(30f);
-				passes.get(2).getProgram().getUniformByNameOrDummy("Radius").set(30f - (time % 3000) / 100f); //using time turns it into a black screen?
+				//TODO: fix the issue that the blur shaders actually change the brightness when given a small, non-integer radius
+				passes.get(1).getProgram().getUniformByNameOrDummy("Radius").set(64f * blur_strength);
+				passes.get(2).getProgram().getUniformByNameOrDummy("Radius").set(16f * blur_strength); //more horizontal blur => primitive diplopia emulation (if can't use custom shaders)
 			}
 		}
-		else
+		else if((player.getWorld().getTime() & 1) == 0)
 		{
 			breakingfemme_invokeClearPostProcessor();
 		}
