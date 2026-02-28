72952f4363a941d3db0b6c0052492c6324a52b4e
Checkmate6659 <73489554+Checkmate6659@users.noreply.github.com>
2026-02-28 13:05:31 +0100

    Removed unnecessary complication/irregularity

--- src/main/java/com/breakingfemme/KineticsAttachments.java
+++ src/main/java/com/breakingfemme/KineticsAttachments.java
@@ -18,11 +18,8 @@ public class KineticsAttachments {
 	static final float MAX_FLOAT = 3.4028235e38f;
 	public static final AttachmentType<Float> BUFFERED_ETHANOL = AttachmentRegistry.createPersistent( //unit: g
 		Identifier.of(BreakingFemme.MOD_ID, "level_buffered_ethanol"), Codec.floatRange(0f, MAX_FLOAT));
-	//public static final AttachmentType<Float> ETHANOL = AttachmentRegistry.createPersistent( //unit: g/L in blood
-	//	Identifier.of(BreakingFemme.MOD_ID, "level_ethanol"), Codec.floatRange(0f, MAX_FLOAT));
-	public static final AttachmentType<Float> ETHANOL = AttachmentRegistry.<Float>builder() //unit: g/L in blood
-		.persistent(Codec.floatRange(0f, MAX_FLOAT))
-		.buildAndRegister(Identifier.of(BreakingFemme.MOD_ID, "level_ethanol"));
+	public static final AttachmentType<Float> ETHANOL = AttachmentRegistry.createPersistent( //unit: g/L in blood
+		Identifier.of(BreakingFemme.MOD_ID, "level_ethanol"), Codec.floatRange(0f, MAX_FLOAT));
 	public static final AttachmentType<Float> ACETALDEHYDE = AttachmentRegistry.createPersistent( //unit: g/L in blood
 		Identifier.of(BreakingFemme.MOD_ID, "level_acetaldehyde"), Codec.floatRange(0f, MAX_FLOAT));
 
