95fd424ca5c489a5597425c33f8f2aebfcfde3a1
Checkmate6659 <73489554+Checkmate6659@users.noreply.github.com>
2026-03-03 18:55:24 +0100

    redid some logic not to use age, still didnt figure out whats up with the forgetting

--- src/main/java/com/breakingfemme/VillagerAttachments.java
+++ src/main/java/com/breakingfemme/VillagerAttachments.java
@@ -8,18 +8,19 @@ import net.fabricmc.fabric.api.attachment.v1.AttachmentType;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
 
 public class VillagerAttachments {
-    public static final AttachmentType<Integer> IS_TRANSFEM = AttachmentRegistry.createPersistent( //is villager transfem? (should be a boolean but that gets forgotten, so we do this instead)
-        Identifier.of(BreakingFemme.MOD_ID, "transfem"), Codec.intRange(0, 1));
+    public static final AttachmentType<Boolean> IS_TRANSFEM = AttachmentRegistry.createPersistent( //is villager transfem? (should be a boolean but that gets forgotten, so we do this instead)
+        Identifier.of(BreakingFemme.MOD_ID, "is_transfem"), Codec.BOOL);
     
     //TODO: add villagers that turn into the agents from the matrix and try to kill you
  
     public static final int MAX_ESTRO_PROGRESS = 288000; //this really is the amount of time the villager needs to transition: about 3h 40min irl
     public static final AttachmentType<Integer> ESTRO_PROGRESS = AttachmentRegistry.createPersistent( //basically like number of doses of estrogen
         Identifier.of(BreakingFemme.MOD_ID, "estro_progress"), Codec.intRange(0, MAX_ESTRO_PROGRESS));
-    public static final AttachmentType<Integer> ESTRO_NEED_AGE = AttachmentRegistry.createPersistent( //age when needing estrogen again
-        Identifier.of(BreakingFemme.MOD_ID, "estro_need_age"), Codec.intRange(0, Integer.MAX_VALUE));
+    public static final AttachmentType<Long> ESTRO_NEED_TIME = AttachmentRegistry.createPersistent( //age when needing estrogen again
+        Identifier.of(BreakingFemme.MOD_ID, "estro_need_time"), Codec.LONG);
 
     //check if an item is an estrogen (for villagers)
     public static boolean isEstrogen(Item item)
@@ -52,27 +53,28 @@ public class VillagerAttachments {
 	{
         //need to set when queried, otherwise it would be a different random pick afterwards!
         //also, the transfem-ness status of a villager isnt changeable after being set like this
-        int is_transfem = (villager.getWorld().getRandom().nextInt(256) < 254) ? 1 : 0;
-		return villager.getAttachedOrSet(IS_TRANSFEM, is_transfem) == 1; //this adjusts the probability of being transfem
+        if(!villager.hasAttached(IS_TRANSFEM))
+        {
+            boolean is_transfem = villager.getWorld().getRandom().nextInt(256) < 254; //this adjusts the probability of being transfem (TODO: set it to sth not absurdly high after it works)
+            villager.setAttached(IS_TRANSFEM, is_transfem);
+        }
+		return villager.getAttached(IS_TRANSFEM);
 	}
 
     public static int getTransitionTime(VillagerEntity villager)
     {
         //ESTRO_PROGRESS: progress after this dose has been consumed
         //ESTRO_NEED_AGE - age: time until the dose is consumed (is 0 if villager needs estrogen)
-        int time_to_next = villager.getAttachedOrSet(ESTRO_NEED_AGE, villager.age) - villager.age;
+        World world = villager.getWorld();
+        long time_to_next = villager.getAttachedOrSet(ESTRO_NEED_TIME, world.getTime()) - world.getTime(); //should be in int values realistically
         if(time_to_next < 0) time_to_next = 0;
-        return villager.getAttachedOrSet(ESTRO_PROGRESS, 0) - time_to_next;
+        return villager.getAttachedOrSet(ESTRO_PROGRESS, 0) - (int)time_to_next;
     }
 
     public static boolean needsEstrogen(VillagerEntity villager)
     {
-        //NOTE: this is NOT equivalent to just doing age >= estro need age. cuz these ages will overflow at some point!
-        //the age variable will overflow after 3 real-life years of being loaded (or less with other mods that can accelerate time)
-        int delta = villager.age - villager.getAttachedOrSet(ESTRO_NEED_AGE, villager.age);
-        if(delta >= Integer.MAX_VALUE / 4) //once every 4.5 months, do sth to stop the villagers from not needing estrogen for 1.5 years
-            villager.setAttached(ESTRO_NEED_AGE, villager.age);
-        return delta >= 0;
+        World world = villager.getWorld();
+        return world.getTime() >= villager.getAttachedOrSet(ESTRO_NEED_TIME, world.getTime());
     }
 
     public static void giveEstrogenFor(VillagerEntity villager, int amount)
@@ -84,6 +86,6 @@ public class VillagerAttachments {
         villager.setAttached(ESTRO_PROGRESS, estro_progress);
 
         //add to time of next dose
-        villager.setAttached(ESTRO_NEED_AGE, villager.age + amount);
+        villager.setAttached(ESTRO_NEED_TIME, villager.getWorld().getTime() + amount);
     }
 }
