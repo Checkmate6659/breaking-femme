1de2735011984681306b71ea0c042c853feacf22
Checkmate6659 <73489554+Checkmate6659@users.noreply.github.com>
2026-03-16 20:34:45 +0100

    Added larger header and custom title

--- src/main/java/com/breakingfemme/mixin/GuideBookMixin.java
+++ src/main/java/com/breakingfemme/mixin/GuideBookMixin.java
@@ -2,7 +2,9 @@ package com.breakingfemme.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.breakingfemme.BreakingFemme;
@@ -13,6 +15,14 @@ import net.minecraft.util.Identifier;
 //https://github.com/VazkiiMods/Patchouli/blob/1.21.x/Xplat/src/main/java/vazkii/patchouli/client/book/gui/GuiBookLanding.java
 @Mixin(GuiBookLanding.class)
 public class GuideBookMixin {
+    @ModifyConstant(method = "drawHeader", constant = @Constant(intValue = 31))
+    private int breakingfemme$largerHeader(int value)
+    {
+        if(((GuiBookLanding)(Object)this).book.name.contentEquals("book.breakingfemme.name"))
+            return 37;
+        return value;
+    }
+
     @Inject(method = "drawHeader", at = @At(value = "INVOKE", target = "drawText"), cancellable = true)
     private void breakingfemme$hijack(CallbackInfo ci)
     {
