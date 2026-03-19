c8065a6b5acc1407daea43fa2695b16ef3c02a8b
Checkmate6659 <73489554+Checkmate6659@users.noreply.github.com>
2026-03-19 16:10:19 +0100

    OMG YES IT WORKS :3 :3 :3

--- src/main/java/com/breakingfemme/block/entity/DistillerBlockEntityRenderer.java
+++ src/main/java/com/breakingfemme/block/entity/DistillerBlockEntityRenderer.java
@@ -3,103 +3,100 @@ package com.breakingfemme.block.entity;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.render.model.BakedQuad;
-import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.render.model.BakedQuadFactory;
 import net.minecraft.client.texture.Sprite;
 
-import com.breakingfemme.fluid.ModFluids;
-import com.breakingfemme.item.ModItems;
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
 
+import com.breakingfemme.BreakingFemme;
+import com.breakingfemme.fluid.ModFluids;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandler;
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
-import net.fabricmc.fabric.api.transfer.v1.client.fluid.FluidVariantRendering;
-import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.RenderLayers;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.item.ItemStack;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ColorHelper;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.RotationAxis;
 import net.minecraft.world.World;
 
+@Environment(EnvType.CLIENT)
 public class DistillerBlockEntityRenderer implements BlockEntityRenderer<DistillerBlockEntity> {
-    //check out VertexConsumer.java: these ints are read as bytes (all little endian) and interpreted as:
-    //x of vertex: float (4 bytes)
-    //y of vertex: float (4 bytes)
-    //z of vertex: float (4 bytes)
-    //r, g and b values (1 byte each)
-    //1 unused byte
-    //u of vertex: float (4 bytes)
-    //v of vertex: float (4 bytes)
-    //8 unused bytes
-    //for a total of 32 bytes, or 8 ints
-    //here we set all vertex y values to be 0, to control the fluid level easier
-    private static int[] VERTEX_DATA = {
-        0x3E000000, 0, 0x3E000000, -1, 0x3E000000, 0x3E000000, 42, 42,
-        0x3E000000, 0, 0x3F600000, -1, 0x3E000000, 0x3F600000, 42, 42,
-        0x3F600000, 0, 0x3F600000, -1, 0x3F600000, 0x3F600000, 42, 42,
-        0x3F600000, 0, 0x3E000000, -1, 0x3F600000, 0x3E000000, 42, 42
-    };
-
-    // A jukebox itemstack
-    private static final ItemStack stack = new ItemStack(ModItems.CHILI_PEPPER);
-    
     public DistillerBlockEntityRenderer(BlockEntityRendererFactory.Context ctx) {}
 
     @Override
     public void render(DistillerBlockEntity blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         matrices.push(); //we need to do this when doing rendering... otherwise the matrices are gonna get fucked after this is rendered
-
-        // Calculate the current offset in the y value
-        double offset = Math.sin((blockEntity.getWorld().getTime() + tickDelta) / 8.0) / 4.0;
-        // Move the item
-        matrices.translate(0.5, 1.25 + offset, 0.5);
- 
-        // Rotate the item
-        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees((blockEntity.getWorld().getTime() + tickDelta) * 4));
-
-        //render the item
-        MinecraftClient.getInstance().getItemRenderer().renderItem(stack, ModelTransformationMode.GROUND, light, overlay, matrices, vertexConsumers, blockEntity.getWorld(), 0);
-
-        matrices.pop(); //get the old matrices back
-
-        //end of test section for checking if the rendering works
-        //TODO: remove code before when done!
-
-        matrices.push(); //we need to do this when doing rendering... otherwise the matrices are gonna get fucked after this is rendered
-        matrices.translate(0, 2.0, 0); //a bit outside the box for now, we'll put it inside later!
+        matrices.translate(0, 2.0, 0); //a bit outside the box for now, we'll put it inside later! and level depends on block entity
 
         BlockPos pos = blockEntity.getPos();
         World world = blockEntity.getWorld();
         VertexConsumer vc = vertexConsumers.getBuffer(RenderLayers.getFluidLayer(ModFluids.STILL_BEER.getStill(false)));
         FluidRenderHandler handler = FluidRenderHandlerRegistry.INSTANCE.get(ModFluids.STILL_SLUDGE);
-        //Sprite sprite = handler.getFluidSprites(world, pos, ModFluids.STILL_SLUDGE.getDefaultState())[0]; //0 is still (in atlas), 1 is flowing
-        Sprite sprite = FluidVariantRendering.getSprite(FluidVariant.of(ModFluids.STILL_COAL_OIL));
+        Sprite sprite = handler.getFluidSprites(world, pos, ModFluids.STILL_SLUDGE.getDefaultState())[0]; //0 is still (in atlas), 1 is flowing
+
+        //BreakingFemme.LOGGER.info("uv on atlas: u: " + sprite.getMinU() + " to " + sprite.getMaxU() + "; v: " + sprite.getMinV() + " to " + sprite.getMaxV());
 
         //we need to recompute the vertex data here, in order not to get the entire atlas but just the texture we want
-        float delta = (sprite.getFrameU(16.0) - sprite.getFrameU(0.0)) * 0.125f; //to not have a tiny bit of fluid poking out on the side
-        float mu = sprite.getFrameU(0.0) + delta;
-        float mv = sprite.getFrameV(0.0) + delta;
-        float Mu = mu + 1 - delta - delta;
-        float Mv = mv + 1 - delta - delta;
-        VERTEX_DATA[8 * 0 + 4] = Float.floatToRawIntBits(mu);
-        VERTEX_DATA[8 * 0 + 5] = Float.floatToRawIntBits(mv);
-        VERTEX_DATA[8 * 1 + 4] = Float.floatToRawIntBits(mu);
-        VERTEX_DATA[8 * 1 + 5] = Float.floatToRawIntBits(Mv);
-        VERTEX_DATA[8 * 2 + 4] = Float.floatToRawIntBits(Mu);
-        VERTEX_DATA[8 * 2 + 5] = Float.floatToRawIntBits(Mv);
-        VERTEX_DATA[8 * 3 + 4] = Float.floatToRawIntBits(Mu);
-        VERTEX_DATA[8 * 3 + 5] = Float.floatToRawIntBits(mv);
+        /*float delta = (sprite.getMaxU() - sprite.getMinU()) * 0.125f; //to not have a tiny bit of fluid poking out on the side
+        float minU = sprite.getMinU() + delta;
+        float minV = sprite.getMinV() + delta;
+        float maxU = minU + 1 - delta - delta;
+        float maxV = minV + 1 - delta - delta;*/
 
-        BakedQuad quad = new BakedQuad(VERTEX_DATA, 0, Direction.UP, sprite, false);
+        float minU = sprite.getMinU(); //test to make sure nothing goes wrong
+        float minV = sprite.getMinV();
+        float maxU = sprite.getMaxU();
+        float maxV = sprite.getMaxV();
+
         int col = handler.getFluidColor(world, pos, ModFluids.STILL_SLUDGE.getDefaultState());
-        vc.quad(matrices.peek(), quad, ColorHelper.Argb.getRed(col) * 0.00390625f, ColorHelper.Argb.getGreen(col) * 0.00390625f, ColorHelper.Argb.getBlue(col) * 0.00390625f, light, overlay);
 
-        //handler.getFluidSprites(world, pos, ModFluids.STILL_SLUDGE.getDefaultState())[0].getMinU().
-        
+        Matrix4f pos_matrix = matrices.peek().getPositionMatrix();
+        Matrix3f normal_matrix = matrices.peek().getNormalMatrix();
+        vc.vertex(pos_matrix, 0, 0, 0)
+            .color(col)
+            .texture(minU, minV)
+            .light(light)
+            .overlay(OverlayTexture.DEFAULT_UV)
+            .normal(normal_matrix, 0, 0, 0)
+            .next();
+
+        vc.vertex(pos_matrix, 0, 0, 1)
+            .color(col)
+            .texture(maxU, minV)
+            .light(light)
+            .overlay(OverlayTexture.DEFAULT_UV)
+            .normal(normal_matrix, 0, 0, 1)
+            .next();
+
+        vc.vertex(pos_matrix, 1, 0, 1)
+            .color(col)
+            .texture(maxU, maxV)
+            .light(light)
+            .overlay(OverlayTexture.DEFAULT_UV)
+            .normal(normal_matrix, 1, 0, 1)
+            .next();
+
+        vc.vertex(pos_matrix, 1, 0, 0)
+            .color(col)
+            .texture(minU, maxV)
+            .light(light)
+            .overlay(OverlayTexture.DEFAULT_UV)
+            .normal(normal_matrix, 1, 0, 0)
+            .next();
+
+        //MinecraftClient.getInstance().getBlockRenderManager().renderFluid(pos.add(1, 1, 1), world, vc, Blocks.AIR.getDefaultState(), Fluids.WATER.getDefaultState());
+
         matrices.pop(); //get the old matrices back
     }
 }
