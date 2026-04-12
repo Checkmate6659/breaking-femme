c57ae111f60c0336d46fd6f839ccda61ee9ecbcc
Checkmate6659 <73489554+Checkmate6659@users.noreply.github.com>
2026-04-12 19:52:09 +0200

    Removed an unused import

--- src/main/java/com/breakingfemme/block/DistillerBaseBlock.java
+++ src/main/java/com/breakingfemme/block/DistillerBaseBlock.java
@@ -25,7 +25,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Pair;
