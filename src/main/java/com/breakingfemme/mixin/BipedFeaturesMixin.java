ea5fca5fd43b97a72b8d65a83c91edf9c1dafd39
Checkmate6659 <73489554+Checkmate6659@users.noreply.github.com>
2026-08-02 14:04:42 +0200

    Added jacket features

--- src/main/java/com/breakingfemme/mixin/BipedFeaturesMixin.java
+++ src/main/java/com/breakingfemme/mixin/BipedFeaturesMixin.java
@@ -18,6 +18,7 @@ import net.minecraft.client.model.Dilation;
 import net.minecraft.client.model.ModelData;
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.model.ModelPartBuilder;
+import net.minecraft.client.model.ModelPartData;
 import net.minecraft.client.model.ModelTransform;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
@@ -27,18 +28,22 @@ import net.minecraft.util.Identifier;
 public class BipedFeaturesMixin {
 	@Unique
    	public ModelPart breakingfemme$features; //can't be final cuz constructor injector is *not* the constructor
-	//TODO: add features jacket layer... if it exists (PlayerEntityModel, VillagerResemblingModel...)
+   	public ModelPart breakingfemme$features_jacket; //jacket layer if it exists (PlayerEntityModel, VillagerResemblingModel...)
 	//TODO: add features armor layer for vanilla-style armor, compat for vanilla-style armor, and compat mappings
-	//TODO: add mob list (no skeletons)
+	//TODO: add mob list (skeletons? theres the whole transparency issue tho...)
 	//for zombie villagers (mb other mobs?) the body's uv is different
+	//also for thicker mobs with crossed arms (villagers and evokers) it needs to be more forward and maybe higher up
 
     @Inject(method = "<init>(Lnet/minecraft/client/model/ModelPart;Ljava/util/function/Function;)V", at = @At("RETURN"))
     private void breakingfemme$constructor_features(ModelPart root, Function<Identifier, RenderLayer> renderLayerFactory, CallbackInfo ci) {
 		if(root.hasChild("breakingfemme_features"))
 			breakingfemme$features = root.getChild("breakingfemme_features");
 		else
-			BreakingFemme.LOGGER.error("ERROR: Breaking Femme features failed to load!");
-    }
+			BreakingFemme.LOGGER.error("ERROR: Some Breaking Femme features failed to load!");
+
+		if(root.hasChild("breakingfemme_features_jacket")) //if not the case, it just doesn't exist and that's normal!
+			breakingfemme$features_jacket = root.getChild("breakingfemme_features_jacket");
+	}
 
     @ModifyReturnValue(method = "getModelData", at = @At("RETURN"))
     private static ModelData breakingfemme$getModelData_features(ModelData data, Dilation dilation, float pivotOffsetY) {
@@ -47,25 +52,38 @@ public class BipedFeaturesMixin {
 		//also how do i make villager have them higher up and more forward or sth, to make them actually show
 		//and not have boobs on skeletons
 		//to adjust boob height, only change pivot y position! (for some odd reason that works)
-		data.getRoot().addChild("breakingfemme_features", ModelPartBuilder.create().uv(18, 22).cuboid(-4.0F, -1.0F, -0.875F, 8.0F, 2.0F, 2.0F, dilation.add(-0.00390625F)), ModelTransform.of(0.0F, 3.0F, -1.75F, 1.0F, pivotOffsetY, 0.0F));
+
+		ModelPartData mpd = data.getRoot();
+		mpd.addChild("breakingfemme_features", ModelPartBuilder.create().uv(18, 22).cuboid(-4.0F, -1.0F, -0.875F, 8.0F, 2.0F, 2.0F, dilation.add(-0.00390625F)), ModelTransform.of(0.0F, 3.0F, -1.75F, 1.0F, pivotOffsetY, 0.0F));
+
+		if(mpd.getChild("jacket") != null)
+			mpd.addChild("breakingfemme_features_jacket", ModelPartBuilder.create().uv(18, 38).cuboid(-4.0F, -1.0F, -0.875F, 8.0F, 2.0F, 2.0F, dilation.add(0.25F - 0.00390625F)), ModelTransform.of(0.0F, 3.0F, -1.75F, 1.0F, pivotOffsetY, 0.0F));
+
 		return data;
     }
 
 	@SuppressWarnings("null")
 	@WrapMethod(method = "getBodyParts")
 	private Iterable<ModelPart> breakingfemme$getBodyParts_features(Operation<Iterable<ModelPart>> original) {
-		return Iterables.concat(ImmutableList.of(breakingfemme$features), original.call());
+		if(breakingfemme$features_jacket == null)
+			return Iterables.concat(ImmutableList.of(breakingfemme$features), original.call());
+		return Iterables.concat(ImmutableList.of(breakingfemme$features, breakingfemme$features_jacket), original.call());
 	}
 
     @Inject(method = "setVisible", at = @At("HEAD"))
     private void breakingfemme$setVisible_features(boolean visible, CallbackInfo ci) {
 		breakingfemme$features.visible = visible;
+		if(breakingfemme$features_jacket != null)
+			breakingfemme$features_jacket.visible = visible;
     }
 
 	//in vanilla this method is only used to draw armor for some reason (need to look into armor rendering at some point)
     @Inject(method = "copyBipedStateTo", at = @At("RETURN"))
     private void breakingfemme$copy_features(BipedEntityModel<?> model, CallbackInfo ci) {
-		((BipedFeaturesMixin)(Object)model).breakingfemme$features.copyTransform(breakingfemme$features);
+		BipedFeaturesMixin model_mixin = (BipedFeaturesMixin)(Object)model;
+		model_mixin.breakingfemme$features.copyTransform(breakingfemme$features);
+		if(breakingfemme$features_jacket != null)
+			model_mixin.breakingfemme$features_jacket.copyTransform(breakingfemme$features_jacket);
     }
 
 	
