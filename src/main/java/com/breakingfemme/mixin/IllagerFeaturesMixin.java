ac7bc77da4dad5701911432acddc6177d35b3dd0
Checkmate6659 <73489554+Checkmate6659@users.noreply.github.com>
2026-08-02 21:38:57 +0200

    Added illager features

---
+++ src/main/java/com/breakingfemme/mixin/IllagerFeaturesMixin.java
@@ -0,0 +1,55 @@
+package com.breakingfemme.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import com.breakingfemme.BreakingFemme;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
+import net.minecraft.client.model.Dilation;
+import net.minecraft.client.model.ModelData;
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.model.ModelPartBuilder;
+import net.minecraft.client.model.ModelTransform;
+import net.minecraft.client.model.TexturedModelData;
+import net.minecraft.client.render.entity.model.IllagerEntityModel;
+
+@Mixin(value = IllagerEntityModel.class)
+public class IllagerFeaturesMixin {
+	@Unique
+   	public ModelPart breakingfemme$features; //can't be final cuz constructor injector is *not* the constructor
+
+	@Unique
+	public ModelPart breakingfemme$features_jacket; //jacket layer, its kinda weird here but we need it.
+   
+    @Inject(method = "<init>(Lnet/minecraft/client/model/ModelPart;)V", at = @At("RETURN"))
+    private void breakingfemme$constructor_features(ModelPart root, CallbackInfo ci) {
+		if(root.hasChild("breakingfemme_features"))
+			breakingfemme$features = root.getChild("breakingfemme_features");
+		else
+			BreakingFemme.LOGGER.error("ERROR: Some Breaking Femme features failed to load!");
+
+		if(root.hasChild("breakingfemme_features_jacket"))
+			breakingfemme$features_jacket = root.getChild("breakingfemme_features_jacket");
+		else
+			BreakingFemme.LOGGER.error("ERROR: Some Breaking Femme covered features failed to load!");
+	}
+
+    @WrapOperation(method = "getTexturedModelData", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/TexturedModelData;of(Lnet/minecraft/client/model/ModelData;II)Lnet/minecraft/client/model/TexturedModelData;"))
+    private static TexturedModelData breakingfemme$getTexturedModelData_features(ModelData data, int w, int h, Operation<TexturedModelData> original) {
+		//NOTE: with this setup we cant move features up/down depending on arm state (crossed or on the side)
+		//we will need to do that in animation code
+		//also the body of the illager has an integrated "jacket" for some reason. so we need to add the jacket in.
+		//we could use that kinda stuff for our jacket actually, would simplify things potentially... maybe.
+		data.getRoot().addChild("breakingfemme_features", ModelPartBuilder.create().uv(20, 26).cuboid(-4.0F, -1.0F, -0.875F, 8.0F, 2.0F, 2.0F, new Dilation(-0.00390625F)), ModelTransform.of(0.0F, 2.0F, -2.75F, 1.0F, 0.0F, 0.0F));
+		data.getRoot().addChild("breakingfemme_jacket_features", ModelPartBuilder.create().uv(4, 44).cuboid(-4.0F, -1.0F, -0.875F, 8.0F, 2.0F, 2.0F, new Dilation(0.25F - 0.00390625F)), ModelTransform.of(0.0F, 2.0F, -2.75F, 1.0F, 0.0F, 0.0F));
+
+		return original.call(data, w, h);
+    }
+
+
+	//TODO: feature growth (position and angle) animation shall be done under setAngles
+}
