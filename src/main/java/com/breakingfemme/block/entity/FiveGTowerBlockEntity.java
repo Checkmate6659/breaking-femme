68239dbdd8e7173dc9763427f0bae7c41cc187fc
Checkmate6659 <73489554+Checkmate6659@users.noreply.github.com>
2026-08-20 11:54:04 +0200

    Made 5G tower taller

--- src/main/java/com/breakingfemme/block/entity/FiveGTowerBlockEntity.java
+++ src/main/java/com/breakingfemme/block/entity/FiveGTowerBlockEntity.java
@@ -7,6 +7,7 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.ServerWorldAccess;
 
 public class FiveGTowerBlockEntity extends BlockEntity {
@@ -14,14 +15,23 @@ public class FiveGTowerBlockEntity extends BlockEntity {
 
     public FiveGTowerBlockEntity( BlockPos pos, BlockState state) {
         super(ModBlockEntities.FIVE_G_TOWER_BLOCK_ENTITY_BLOCK_ENTITY, pos, state);
-        topPosition = pos.up(10);
+        topPosition = pos.up(32);
     }
 
     public void tick(ServerWorldAccess server) {
-        var  nearEntities = server.getOtherEntities(null, Box.of(topPosition.toCenterPos(),20,20,20), new EntityAttachments.IsEstrogennablePredicate());
+        //TODO: only do multiblock checking etc once every few ticks (like 20 or 40)
+        //and pick equivalence class for each ticked 5g tower just like the fermenters
+
+        var nearEntities = server.getOtherEntities(null, Box.of(topPosition.toCenterPos(),256,256,256), new EntityAttachments.IsEstrogennablePredicate());
         for (Entity nearEntity : nearEntities) {
-            EntityAttachments.giveEstrogenFor(nearEntity,5);
+            Vec3d delta = nearEntity.getPos().subtract(topPosition.toCenterPos());
+
+            //for now action zone is a sphere. TODO: anisotropy!! like distance to a circle around the top instead
+            //a bit like this: https://www.geometrictools.com/Documentation/DistanceToCircle3.pdf
+            double dist2 = nearEntity.squaredDistanceTo(delta);
+            if(dist2 > 16384) continue;
+
+            EntityAttachments.giveEstrogenFor(nearEntity,5); //TODO: decay of effectiveness based on distance!
         }
     }
-
 }
