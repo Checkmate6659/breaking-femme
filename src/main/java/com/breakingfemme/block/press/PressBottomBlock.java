8788ba10773e991c15dfbb0512e7d581495c8310
Checkmate6659 <73489554+Checkmate6659@users.noreply.github.com>
2026-08-26 22:39:00 +0200

    Added model & texture for press bottom, added horizontal facing property

--- src/main/java/com/breakingfemme/block/press/PressBottomBlock.java
+++ src/main/java/com/breakingfemme/block/press/PressBottomBlock.java
@@ -1,19 +1,76 @@
 package com.breakingfemme.block.press;
 
 import com.breakingfemme.block.entity.press.PressBottomBlockEntity;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+
 import org.jetbrains.annotations.Nullable;
 
 public class PressBottomBlock extends BlockWithEntity {
+    public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
+    private static final VoxelShape SHAPE;
+
     public PressBottomBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        super.appendProperties(builder);
+        builder.add(FACING);
+    }
+
+    //2 functions from HorizontalFacingBlock
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        BlockState blockState = this.getDefaultState();
+        blockState = blockState.with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+        return blockState;
+    }
+
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
     }
 
     @Override
     public @Nullable BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
         return new PressBottomBlockEntity(pos, state);
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state)
+    {
+        return BlockRenderType.MODEL;
+    }
+
+    static {
+        //TODO: complete shape!
+        SHAPE = VoxelShapes.union(
+            Block.createCuboidShape(1.0, 0.0, 1.0, 15.0, 1.0, 15.0)
+        );
     }
 }
